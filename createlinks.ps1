4f8a481cbea0dce2a29dd4f0ee03ef5f89f40723
Silas-K <silas.koehnke@student.jade-hs.de>
2024-05-01 00:55:10 +0200

    update theme location and add environment variables

--- createlinks.ps1
+++ createlinks.ps1
@@ -11,3 +11,7 @@ foreach ($symlink in $symlinks.GetEnumerator())
     Get-Item -Path $symlink.Key -ErrorAction SilentlyContinue | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
     New-Item -ItemType SymbolicLink -Path $symlink.Key -Target (Resolve-Path $symlink.Value) -Force | Out-Null
 }
+
+# Persist Environment Variables (directory for nvim)
+[System.Environment]::SetEnvironmentVariable('nvimhome', "$($env:LocalAppData)\nvim", [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('psconfighome', "$($PSScriptRoot)", [System.EnvironmentVariableTarget]::User)
--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,5 @@
 Set-Alias vi nvim
 Set-Alias vim nvim
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\capr4n_power10k_modified_2lines.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\capr4n_power10k_modified_2lines.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:psconfighome\capr4n_power10k_modified_2lines.omp.json" | Invoke-Expression
 Import-Module git-aliases -DisableNameChecking
